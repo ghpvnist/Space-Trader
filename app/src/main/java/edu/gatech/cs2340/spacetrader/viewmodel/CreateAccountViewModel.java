f210eea139e99a81225d503fe95a558a9ab75466
wange011 <wange011@gmail.com>
2019-02-15 17:47:56 -0500

    add viewmodel for CreateAccount

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/viewmodel/CreateAccountViewModel.java
@@ -0,0 +1,31 @@
+package edu.gatech.cs2340.spacetrader.viewmodel;
+
+import edu.gatech.cs2340.spacetrader.entity.DifficultyLevel;
+import edu.gatech.cs2340.spacetrader.model.Player;
+
+public class CreateAccountViewModel {
+
+    private DifficultyLevel difficulty;
+    private Player player;
+
+    public CreateAccountViewModel() {
+        this.difficulty = DifficultyLevel.NORMAL;
+        this.player = new Player();
+    }
+
+    public DifficultyLevel getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(DifficultyLevel difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
