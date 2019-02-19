6d36e980f60719817bf6281aa5d2a31ba1bd8323
Gunhyun Park <ghpvnist28@gmail.com>
2019-02-19 00:41:09 -0500

    Add splash screen

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/view/SplashActivity.java
@@ -0,0 +1,18 @@
+package edu.gatech.cs2340.spacetrader.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
