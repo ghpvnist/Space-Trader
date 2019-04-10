533896cfd055032d7be5eb7be7d3d603a420ffcb
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2019-04-10 14:37:55 -0400

    Add WIP Universe2DView

---
+++ app/src/main/java/edu/gatech/cs2340/spacetrader/Universe2DViewActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.cs2340.spacetrader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Universe2DViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_universe2_dview);
+    }
+}
